3c6f14fee44e8528ce4e356667c15f271fbadb2d
Eric Friday <ericfriday@gmail.com>
2017-12-06 22:19:25 -0600

    Update and rename aoc-day-4 to aoc-day-4.js

--- aoc-day-4.js
+++ aoc-day-4.js
@@ -16,3 +16,12 @@ const input = document.body.textContent.trim().split('\n'),
 
 console.log(sumID(realRooms), poleID(realRooms));
 
+
+// better solution with both part 1 & 2
+const phrases = document.body.textContent.trim().split('\n');
+const noRepeats = (w, _, ws) => ws.filter(v => v === w).length === 1;
+const sortLetters = w => [...w].sort().join('');
+const isValid = f => ph => ph.split(' ').map(f).every(noRepeats);
+const count = f => phrases.filter(isValid(f)).length;
+
+console.log([w => w, sortLetters].map(count));
